4f5a45d27d5b132e3a11e2494fb4668b1c844c7b
Mark Videon <>
2022-01-30 13:49:54 +1030

    Implemented refactor of UI - includes implementation of Cancel event, removing duplicate code

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,9 +10,11 @@ public class PlayerMovement : MonoBehaviour
     float horizontal, vertical;
     GameClock gameClock;
     float targetSpeed;
+
+    [SerializeField] private Menu _activeMenu;
     private Pause _pausePanel;
-    [SerializeField] private Win _winPanel;
-    [SerializeField] private Lose _losePanel;
+    private Win _winPanel;
+    private Lose _losePanel;
 
     private void Awake()
     {
@@ -34,8 +36,15 @@ public class PlayerMovement : MonoBehaviour
     {
         if (ctx.performed)
         {
-            _pausePanel.ShowPause();
+            _activeMenu = _pausePanel;
+            _pausePanel.ShowMenu();
         }
+    }
+
+    public void OnCancelUI(InputAction.CallbackContext ctx)
+    {
+        _activeMenu.HideMenu();
+        _activeMenu = null;
     }
 
     public void OnTimeSwitch(InputAction.CallbackContext ctx)
@@ -50,17 +59,19 @@ public class PlayerMovement : MonoBehaviour
     
     public void Pause()
     {
-        _pausePanel.ShowPause();
+        _pausePanel.ShowMenu();
     }
 
     public void OnLose()
     {
-        _losePanel.ShowLose();
+        _activeMenu = _losePanel;
+        _losePanel.ShowMenu();
     }
     
     public void OnWin()
     {
-        _winPanel.ShowWin();
+        _activeMenu = _winPanel;
+        _winPanel.ShowMenu();
     }
 
     public void RegisterLoseMenu(Lose lose)

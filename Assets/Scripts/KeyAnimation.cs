c5c50f2ecc3e4d029de53896e3ce3f76e1fb9730
Sean Mansell <thearknano@gmail.com>
2022-02-06 12:56:50 +1030

    add portal animation

--- Assets/Scripts/KeyAnimation.cs
+++ Assets/Scripts/KeyAnimation.cs
@@ -2,16 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyAnimationFrame : Frame
-{
-    public KeyAnimationFrame(Sprite sprite)
-    {
-        this.sprite = sprite;
-    }
-    public Sprite sprite;
-}
-
-public class KeyAnimation : BufferedState<TowerAnimation, TowerAnimationFrame>
+public class KeyAnimation : MonoBehaviour
 {
     public float animationSpeed;
     public Sprite activeIdleSprite;
@@ -29,27 +20,13 @@ public class KeyAnimation : BufferedState<TowerAnimation, TowerAnimationFrame>
 
     private void Start()
     {
-        base.Start();
         gameClock = FindObjectOfType<GameClock>();
-        SetOnRead((Frame) =>
-        {
-            spriteRenderer.sprite = Frame.sprite;
-        });
-        SetOnWrite(() =>
-        {
-            var newFrame = new TowerAnimationFrame(spriteRenderer.sprite);
-            Record(newFrame);
-        });
+
     }
 
-    private void FixedUpdate()
-    {
-        base.FixedUpdate();
-    }
 
     private void Update()
     {
-        if (gameClock.flow == FlowDirection.backward) return;
         if (!animating) return;
         animationSpeedTimer += Time.deltaTime * gameClock.flowRate;
         if (animationSpeedTimer >= animationSpeed)
@@ -62,7 +39,6 @@ public class KeyAnimation : BufferedState<TowerAnimation, TowerAnimationFrame>
             }
             else
             {
-                print(currentSpriteIndex);
                 spriteRenderer.sprite = activateSprites[currentSpriteIndex];
             }
             animationSpeedTimer = 0f;
